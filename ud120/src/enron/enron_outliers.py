eec14132297121e8486f40701dbfc90d34ca2ca6
Left Thomas <leftthomas@qq.com>
2017-02-28 22:17:42 +0800

    异常值处理

---
+++ ud120/src/enron/enron_outliers.py
@@ -0,0 +1,22 @@
+import pickle
+
+import matplotlib.pyplot as plt
+
+from enron.feature_format import featureFormat
+
+# read in data dictionary, convert to numpy array
+data_dict = pickle.load(open("../../resources/enron_dataset.pkl", "rb"))
+
+data_dict.pop('TOTAL', 0)
+
+features = ["salary", "bonus"]
+data = featureFormat(data_dict, features)
+
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    plt.scatter(salary, bonus)
+
+plt.xlabel("salary")
+plt.ylabel("bonus")
+plt.show()
--- ud120/src/enron/outlier_cleaner.py
+++ ud120/src/enron/outlier_cleaner.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-
-
+import heapq
 def outlierCleaner(predictions, ages, net_worths):
     """
         Clean away the 10% of points that have the largest
@@ -12,8 +10,13 @@ def outlierCleaner(predictions, ages, net_worths):
     """
 
     cleaned_data = []
+    largest_datas = abs(predictions - net_worths)
+    indexs = heapq.nlargest(int(len(predictions) * 0.1), range(len(largest_datas)), largest_datas.take)
 
-    ### your code goes here
-
-
+    i = 0
+    while i < len(predictions):
+        if i not in indexs:
+            cleaned_data.append((ages[i], net_worths[i], predictions[i]))
+        i += 1
+    # print(len(cleaned_data))
     return cleaned_data
