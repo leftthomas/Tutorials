7da757b4f34fb8379c3e188ff92ea07b98ee8240
Left Thomas <leftthomas@qq.com>
2017-02-28 20:34:14 +0800

    线性回归测试

--- ud120/src/enron/regression_classification.py
+++ ud120/src/enron/regression_classification.py
@@ -11,7 +11,7 @@
 import pickle
 
 import matplotlib.pyplot as plt
-from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 from enron.feature_format import featureFormat, targetFeatureSplit
@@ -21,7 +21,8 @@ dictionary = pickle.load(open("../../resources/dataset_modified.pkl", "rb"))
 # list the features you want to look at--first item in the
 # list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat(dictionary, features_list, remove_any_zeroes=True)
+# features_list = ["bonus", "long_term_incentive"]
+data = featureFormat(dictionary, features_list, remove_any_zeroes=True,sort_keys = '../../resources/python2_lesson06_keys.pkl')
 target, features = targetFeatureSplit(data)
 
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5,
@@ -29,11 +30,12 @@ feature_train, feature_test, target_train, target_test = train_test_split(featur
 train_color = "b"
 test_color = "r"
 
-reg = linear_model.LinearRegression()
+reg = LinearRegression()
 reg.fit(feature_train, target_train)
 print(reg.coef_)
 print(reg.intercept_)
 print(reg.score(feature_test, target_test))
+print(reg.score(feature_train, target_train))
 
 # draw the scatterplot, with color-coded training and testing points
 for feature, target in zip(feature_test, target_test):
@@ -50,6 +52,12 @@ try:
     plt.plot(feature_test, reg.predict(feature_test))
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print(reg.coef_)
+print(reg.intercept_)
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
