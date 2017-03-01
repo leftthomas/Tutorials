9176b9abb0fc4b7c99a60989ae7a3047b6aedd1a
Left Thomas <leftthomas@qq.com>
2017-03-01 20:30:03 +0800

    人脸识别案例

--- ud120/src/eigenfaces.py
+++ ud120/src/eigenfaces.py
@@ -13,27 +13,22 @@ The dataset used in this example is a preprocessed excerpt of the
   original source: http://scikit-learn.org/stable/auto_examples/applications/face_recognition.html
 
 """
-
-print
-__doc__
-
 import logging
 from time import time
 
 import numpy as np
 import pylab as pl
-from sklearn.cross_validation import train_test_split
 from sklearn.datasets import fetch_lfw_people
-from sklearn.decomposition import RandomizedPCA
-from sklearn.grid_search import GridSearchCV
+from sklearn.decomposition import PCA
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
 # Display progress logs on stdout
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
-###############################################################################
 # Download the data, if not already on disk and load it as numpy arrays
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
 
@@ -51,46 +46,34 @@ y = lfw_people.target
 target_names = lfw_people.target_names
 n_classes = target_names.shape[0]
 
-print
-"Total dataset size:"
-print
-"n_samples: %d" % n_samples
-print
-"n_features: %d" % n_features
-print
-"n_classes: %d" % n_classes
+print("Total dataset size:")
+print("n_samples: %d" % n_samples)
+print("n_features: %d" % n_features)
+print("n_classes: %d" % n_classes)
 
-###############################################################################
 # Split into a training and testing set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
+# [10, 15, 25, 50, 100, 250]
 n_components = 150
 
-print
-"Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
+print("Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0]))
 t0 = time()
-pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
-print
-"done in %0.3fs" % (time() - t0)
+pca = PCA(n_components=n_components, whiten=True, svd_solver='randomized').fit(X_train)
+print("done in %0.3fs" % (time() - t0))
 
 eigenfaces = pca.components_.reshape((n_components, h, w))
 
-print
-"Projecting the input data on the eigenfaces orthonormal basis"
+print("Projecting the input data on the eigenfaces orthonormal basis")
 t0 = time()
 X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
-print
-"done in %0.3fs" % (time() - t0)
+print("done in %0.3fs" % (time() - t0))
 
-###############################################################################
 # Train a SVM classification model
-
-print
-"Fitting the classifier to the training set"
+print("Fitting the classifier to the training set")
 t0 = time()
 param_grid = {
     'C': [1e3, 5e3, 1e4, 5e4, 1e5],
@@ -99,32 +82,21 @@ param_grid = {
 # for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
 clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
 clf = clf.fit(X_train_pca, y_train)
-print
-"done in %0.3fs" % (time() - t0)
-print
-"Best estimator found by grid search:"
-print
-clf.best_estimator_
+print("done in %0.3fs" % (time() - t0))
+print("Best estimator found by grid search:")
+print(clf.best_estimator_)
 
-###############################################################################
 # Quantitative evaluation of the model quality on the test set
-
-print
-"Predicting the people names on the testing set"
+print("Predicting the people names on the testing set")
 t0 = time()
 y_pred = clf.predict(X_test_pca)
-print
-"done in %0.3fs" % (time() - t0)
+print("done in %0.3fs" % (time() - t0))
 
-print
-classification_report(y_test, y_pred, target_names=target_names)
-print
-confusion_matrix(y_test, y_pred, labels=range(n_classes))
+print(classification_report(y_test, y_pred, target_names=target_names))
+print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
 
 
-###############################################################################
 # Qualitative evaluation of the predictions using matplotlib
-
 def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
     """Helper function to plot a gallery of portraits"""
     pl.figure(figsize=(1.8 * n_col, 2.4 * n_row))
@@ -138,7 +110,6 @@ def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
 
 
 # plot the result of the prediction on a portion of the test set
-
 def title(y_pred, y_test, target_names, i):
     pred_name = target_names[y_pred[i]].rsplit(' ', 1)[-1]
     true_name = target_names[y_test[i]].rsplit(' ', 1)[-1]
@@ -151,8 +122,12 @@ prediction_titles = [title(y_pred, y_test, target_names, i)
 plot_gallery(X_test, prediction_titles, h, w)
 
 # plot the gallery of the most significative eigenfaces
-
 eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
 plot_gallery(eigenfaces, eigenface_titles, h, w)
 
 pl.show()
+
+print(pca.explained_variance_ratio_[0])
+print(pca.explained_variance_ratio_[1])
+print(pca.explained_variance_[0])
+print(pca.explained_variance_[1])
