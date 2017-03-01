3aec788d8db13e0809553e6b32d901c5df22de89
Left Thomas <leftthomas@qq.com>
2017-03-01 18:28:39 +0800

    tfldf有些没有做，因为一直搞不懂特征数到底是哪个参数，特征值又是哪个参数

--- ud120/src/vectorize_text.py
+++ ud120/src/vectorize_text.py
@@ -39,20 +39,22 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         # temp_counter += 1
         # if temp_counter < 200:
         path = os.path.join('../../..', path[:-1])
-            print(path)
-            email = open(path, "r")
+        print(path)
+        email = open(path, "r")
 
-            # use parseOutText to extract the text from the opened email
+        # use parseOutText to extract the text from the opened email
         text = parse_out_email_text.parseOutText(email)
-            # use str.replace() to remove any instances of the words
-            # ["sara", "shackleton", "chris", "germani"]
+        # use str.replace() to remove any instances of the words
+        # ["sara", "shackleton", "chris", "germani"]
+
+        # print(type(text))
         text = text.replace('sara', '')
         text = text.replace('shackleton', '')
         text = text.replace('chris', '')
         text = text.replace('germani', '')
-            # append the text to word_data
+        # append the text to word_data
         word_data.append(text)
-            # append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        # append a 0 to from_data if email is from Sara, and 1 if email is from Chris
         if name == 'sara':
             from_data.append(0)
         else:
