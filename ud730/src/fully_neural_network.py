01ce4907531b169538e733b3952311b4dad29f30
Left Thomas <leftthomas@qq.com>
2017-03-09 01:35:32 +0800

    创建FNN

---
+++ ud730/src/fully_neural_network.py
@@ -0,0 +1,48 @@
+'''
+Assignment 2
+
+Previously in notmnist, we created a pickle with formatted datasets for training,
+development and testing on the notMNIST dataset.
+
+The goal of this assignment is to progressively train deeper and more accurate
+models using TensorFlow.
+'''
+import pickle
+
+import numpy as np
+
+# First reload the data we generated in notmnist
+pickle_file = '../../../notMNIST.pickle'
+
+with open(pickle_file, 'rb') as f:
+    save = pickle.load(f)
+    train_dataset = save['train_dataset']
+    train_labels = save['train_labels']
+    valid_dataset = save['valid_dataset']
+    valid_labels = save['valid_labels']
+    test_dataset = save['test_dataset']
+    test_labels = save['test_labels']
+    del save  # hint to help gc free up memory
+    print('Training set', train_dataset.shape, train_labels.shape)
+    print('Validation set', valid_dataset.shape, valid_labels.shape)
+    print('Test set', test_dataset.shape, test_labels.shape)
+
+# Reformat into a shape that's more adapted to the models we're going to train:
+# data as a flat matrix, labels as float 1-hot encodings.
+image_size = 28
+num_labels = 10
+
+
+def reformat(dataset, labels):
+    dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+    # Map 0 to [1.0, 0.0, 0.0 ...], 1 to [0.0, 1.0, 0.0 ...]
+    labels = (np.arange(num_labels) == labels[:, None]).astype(np.float32)
+    return dataset, labels
+
+
+train_dataset, train_labels = reformat(train_dataset, train_labels)
+valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+test_dataset, test_labels = reformat(test_dataset, test_labels)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
