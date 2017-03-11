63031ed8fd4ce04b9de35191dcbcd81ca935ad3d
Left Thomas <leftthomas@qq.com>
2017-03-12 00:29:59 +0800

    gaussian函数新生成函数

--- cs373/src/gaussian.py
+++ cs373/src/gaussian.py
@@ -1,7 +1,3 @@
-# For this problem, you aren't writing any code.
-# Instead, please just change the last argument
-# in f() to maximize the output.
-
 from math import *
 
 
@@ -9,5 +5,21 @@ def f(mu, sigma2, x):
     return 1 / sqrt(2 * pi * sigma2) * exp(-0.5 * (x - mu) ** 2 / sigma2)
 
 
-# Change the 8. to something else!
+# Instead, please just change the last argument
+# in f() to maximize the output.
 print(f(10, 4, 10))
+
+
+# Write a program to update your mean and variance
+# when given the mean and variance of your belief
+# and the mean and variance of your measurement.
+# This program will update the parameters of your
+# belief function.
+def update(mean1, var1, mean2, var2):
+    # 注意，算new_mean的时候是var1跟mean2、var2跟mean1相乘，别弄反了
+    new_mean = 1 / (var1 + var2) * (var1 * mean2 + var2 * mean1)
+    new_var = 1 / (1 / var1 + 1 / var2)
+    return [new_mean, new_var]
+
+
+print(update(10, 8, 13, 2))
