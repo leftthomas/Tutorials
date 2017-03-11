e4ef35a8b10b732c35cb560d3bcb2b6adb6cefa5
Left Thomas <leftthomas@qq.com>
2017-03-12 00:56:13 +0800

    kalman filter

--- cs373/src/gaussian.py
+++ cs373/src/gaussian.py
@@ -1,6 +1,5 @@
 from math import *
 
-
 def f(mu, sigma2, x):
     return 1 / sqrt(2 * pi * sigma2) * exp(-0.5 * (x - mu) ** 2 / sigma2)
 
@@ -21,5 +20,30 @@ def update(mean1, var1, mean2, var2):
     new_var = 1 / (1 / var1 + 1 / var2)
     return [new_mean, new_var]
 
-
 print(update(10, 8, 13, 2))
+
+
+def predict(mean1, var1, mean2, var2):
+    new_mean = mean1 + mean2
+    new_var = var1 + var2
+    return [new_mean, new_var]
+
+
+print(predict(10, 4, 12, 4))
+
+# Write a program that will iteratively update and
+# predict based on the location measurements
+# and inferred motions shown below.
+measurements = [5., 6., 7., 9., 10.]
+motion = [1., 1., 2., 1., 1.]
+measurement_sig = 4.
+motion_sig = 2.
+mu = 0.
+sig = 10000.
+# Please print out ONLY the final values of the mean
+# and the variance in a list [mu, sig].
+
+for i in range(len(measurements)):
+    [mu, sig] = update(measurements[i], measurement_sig, mu, sig)
+    [mu, sig] = predict(mu, sig, motion[i], motion_sig)
+print([mu, sig])
