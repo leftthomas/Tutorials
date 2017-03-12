a5602f4c2f9fed2c032cf602a22812288a206b0a
Left Thomas <leftthomas@qq.com>
2017-03-12 22:12:12 +0800

    实现search的expand

--- cs373/src/search.py
+++ cs373/src/search.py
@@ -36,13 +36,18 @@ def search(grid, init, goal, cost):
     open_set = []
     close_set = []
     g_values = []
+    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
     path = []
     open_set.append(init)
     g_values.append(0)
+    expand_num = 0
     while len(open_set) > 0:
         min_value = min(g_values)
         min_value_index = g_values.index(min_value)
         min_value_loc = open_set[min_value_index]
+        # 修改expand表
+        expand[min_value_loc[0]][min_value_loc[1]] = expand_num
+        expand_num += 1
         if min_value_loc == goal:
             path = [min_value, min_value_loc[0], min_value_loc[1]]
             break
@@ -55,17 +60,18 @@ def search(grid, init, goal, cost):
             maybe_loc = []
             for d in delta:
                 maybe_loc.append([min_value_loc[0] + d[0], min_value_loc[1] + d[1]])
-            # 判断是否是有效的loc以及是否已经在close集中
             for loc in maybe_loc:
+                # 判断是否是有效的loc
                 if 0 <= loc[0] <= len(grid) - 1 and 0 <= loc[1] <= len(grid[0]) - 1:
-                    if grid[loc[0]][loc[1]] != 1 and loc not in close_set:
+                    # 判断是否已经在open、close集中并且不是障碍物
+                    if grid[loc[0]][loc[1]] != 1 and loc not in close_set and loc not in open_set:
                         # 将有效loc添加进open集与g_values中（g_values记得+cost）
                         open_set.append(loc)
                         g_values.append(min_value + cost)
+    print(expand)
     if len(path) == 0:
         return 'fail'
     else:
         return path
-
 
 print(search(grid, init, goal, cost))
