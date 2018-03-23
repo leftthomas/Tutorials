465466a5bbcae4092d9a0da7777d6d353e6675d5
leftthomas <leftthomas@qq.com>
2018-03-23 21:45:33 +0800

    add LSTM

--- PyTorch/NLP/LSTM.py
+++ PyTorch/NLP/LSTM.py
@@ -1,21 +1,86 @@
 import torch
 from torch import nn
+from torch import optim
 from torch.autograd import Variable
 
-lstm = nn.LSTM(3, 3)
-# make a sequence of length 5
-inputs = [Variable(torch.randn((1, 3))) for _ in range(5)]
-# initialize the hidden state.
-hidden = (Variable(torch.randn(1, 1, 3)), Variable(torch.randn((1, 1, 3))))
 
-for i in inputs:
-    # Step through the sequence one element at a time.
-    # after each step, hidden contains the hidden state.
-    out, hidden = lstm(i.view(1, 1, -1), hidden)
+class LSTMTagger(nn.Module):
 
-inputs = torch.cat(inputs).view(len(inputs), 1, -1)
-# clean out hidden state
-hidden = (Variable(torch.randn(1, 1, 3)), Variable(torch.randn((1, 1, 3))))
-out, hidden = lstm(inputs, hidden)
-print(out)
-print(hidden)
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+        super(LSTMTagger, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        return Variable(torch.zeros(1, 1, self.hidden_dim)), Variable(torch.zeros(1, 1, self.hidden_dim))
+
+    def forward(self, sentence):
+        embeds = self.word_embeddings(sentence)
+        lstm_out, self.hidden = self.lstm(embeds.view(len(sentence), 1, -1), self.hidden)
+        tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
+        return tag_space
+
+
+def prepare_sequence(seq, to_ix):
+    idxs = [to_ix[w] for w in seq]
+    tensor = torch.LongTensor(idxs)
+    return Variable(tensor)
+
+
+training_data = [
+    ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
+    ("Everybody read that book".split(), ["NN", "V", "DET", "NN"])
+]
+
+if __name__ == '__main__':
+    lstm = nn.LSTM(3, 3)
+    # make a sequence of length 5
+    inputs = [Variable(torch.randn((1, 3))) for _ in range(5)]
+    # initialize the hidden state.
+    hidden = (Variable(torch.randn(1, 1, 3)), Variable(torch.randn((1, 1, 3))))
+
+    for i in inputs:
+        # Step through the sequence one element at a time.
+        # after each step, hidden contains the hidden state.
+        out, hidden = lstm(i.view(1, 1, -1), hidden)
+
+    inputs = torch.cat(inputs).view(len(inputs), 1, -1)
+    # clean out hidden state
+    hidden = (Variable(torch.randn(1, 1, 3)), Variable(torch.randn((1, 1, 3))))
+    out, hidden = lstm(inputs, hidden)
+    print(out)
+    print(hidden)
+
+    word_to_ix = {}
+    for sent, tags in training_data:
+        for word in sent:
+            if word not in word_to_ix:
+                word_to_ix[word] = len(word_to_ix)
+    print(word_to_ix)
+    tag_to_ix = {"DET": 0, "NN": 1, "V": 2}
+
+    EMBEDDING_DIM = 6
+    HIDDEN_DIM = 6
+
+    model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix))
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+    for epoch in range(100):
+        for sentence, tags in training_data:
+            model.zero_grad()
+            model.hidden = model.init_hidden()
+            sentence_in = prepare_sequence(sentence, word_to_ix)
+            targets = prepare_sequence(tags, tag_to_ix)
+            tag_scores = model(sentence_in)
+            loss = loss_function(tag_scores, targets)
+            loss.backward()
+            optimizer.step()
+
+    inputs = prepare_sequence(training_data[0][0], word_to_ix)
+    tag_scores = model(inputs)
+    print(tag_scores)
